90d10c8b7b0b03e3b716bee6a7a26b8771dd589c
roberto <roberto.polegato@libero.it>
2024-08-27 06:09:12 +0000

    update project

--- PRJ-Portfolio/functions_etf.py
+++ PRJ-Portfolio/functions_etf.py
@@ -168,7 +168,7 @@ def listEtfCountries(isin):
 
 
 
-#print(listEtfCountries('LU1681048630'))
+print(listEtfCountries('IE00B466KX20'))
 
 #############################################
 #Sempre da extraEtf prendo il paese delle azioni
