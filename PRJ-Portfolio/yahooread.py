73b8b69ef637d4433c5c011efa1977347497eaba
roberto <roberto.polegato@libero.it>
2024-09-12 10:01:50 +0000

    update project

--- PRJ-Portfolio/yahooread.py
+++ PRJ-Portfolio/yahooread.py
@@ -25,6 +25,10 @@ def readYahooSite(tick):
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument("--enable-javascript")
   chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.add_argument("enable-automation")
+  chrome_options.add_argument("--disable-extensions")
+  chrome_options.add_argument("--dns-prefetch-disable")
+  chrome_options.add_argument("--disable-gpu")
 
   driverExt = webdriver.Chrome( options=chrome_options)
   driverExt.get(URL)
