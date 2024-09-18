19b730dd68f4149d777fc4cd89028399d63ff414
roberto <roberto.polegato@libero.it>
2024-09-18 17:17:53 +0000

    update project

--- PRJ-Portfolio/yahooread.py
+++ PRJ-Portfolio/yahooread.py
@@ -27,13 +27,9 @@ def readYahooSite(tick):
   chrome_options.add_argument('--disable-dev-shm-usage')
   chrome_options.add_argument("--disable-gpu")
   chrome_options.add_argument("--disable-extensions")
-
-
-
-  #chrome_options.add_argument("enable-automation")
-  #
-  #chrome_options.add_argument("--dns-prefetch-disable")
-  #chrome_options.add_argument("--disable-gpu")
+  chrome_options.add_argument("enable-automation")
+  chrome_options.add_argument("--dns-prefetch-disable")
+  chrome_options.add_argument("--disable-gpu")
 
 
   driverExt = webdriver.Chrome( options=chrome_options)
