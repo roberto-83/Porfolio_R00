ca71077d7eb5fb3ce5c2e3a48ee2d293b934d6f7
roberto <roberto.polegato@libero.it>
2025-03-04 12:28:35 +0000

    update project

--- PRJ-Portfolio/classes.py
+++ PRJ-Portfolio/classes.py
@@ -573,22 +573,30 @@ class Portfolio:
     histDf.loc['total']= histDf.sum()
     histDf.loc[histDf.index[-1], 'dataHist'] = ''
     histDf.loc[histDf.index[-1], 'Ticker'] = ''
-    #mercato
+    #mercato e vwce
     dataDelta = (datetime.strptime(histDf['dataHist'].iloc[0], '%Y-%m-%d')+timedelta(days=5)).strftime('%Y-%m-%d')
     prezzoMerc1 = yf.download('CSSPX.MI',histDf['dataHist'].iloc[0],dataDelta,progress=False) 
+    prezzoMerc1_vwce = yf.download('VWCE.MI',histDf['dataHist'].iloc[0],dataDelta,progress=False) 
     print(f'Verifico se ci sono dati pre bonifica per ticker {histDf["dataHist"].iloc[0]}')
     print(prezzoMerc1)
     prezzoMerc2 = prezzoMerc1.asfreq('D')
+    prezzoMerc2_vwce = prezzoMerc1_vwce.asfreq('D')
     #prezzoMerc = prezzoMerc2.fillna(method='ffill')
     prezzoMerc = prezzoMerc2.ffill()
+    prezzoMerc_vwce = prezzoMerc2_vwce.ffill()
     rendim=histDf.loc['total']['ctvMerc'] - histDf.loc['total']['TotInvest']
     rendimperc= (rendim * 100) / histDf.loc['total']['TotInvest']
     print(f'Verifico se ci sono dati dopo bonifica per ticker {histDf["dataHist"].iloc[0]}')
     print(prezzoMerc['Close'])
     mercato_df = prezzoMerc['Close'].iloc[0]
+    mercato_df_vwce = prezzoMerc_vwce['Close'].iloc[0]
     mercato = mercato_df['CSSPX.MI']
+    mercato_vwce = mercato_df_vwce['VWCE.MI']
     rendimMerc=100*(float(mercato)-257.01)/257.01
+    rendimMerc_vwce=100*(float(mercato_vwce)-78.620003)/78.620003
     delta = rendimMerc - rendimperc
+    delta_vwce = rendimMerc_vwce - rendimperc
+
     arrTot = [[histDf['dataHist'].iloc[0],
       histDf.loc['total']['TotInvest'],
       histDf.loc['total']['ctvMerc'],
@@ -597,9 +605,10 @@ class Portfolio:
       histDf.loc['total']['Divid'],
       rendimMerc,
       mercato,
-      delta]]
+      delta,mercato_vwce,rendimMerc_vwce,delta_vwce]]
+
     print(f'Array totale del giorno {arrTot}')
-    appendRow('tab_caltot!A:I',arrTot,newPrj)
+    appendRow('tab_caltot!A:L',arrTot,newPrj)
     #write_range('tab_caltot!A'+str(i)+':I'+str(i),arrTot,newPrj)
     
     return 'ok'
