f736c8e8dfbadf47d694c7a85856267e4f7b11d9
roberto <roberto.polegato@libero.it>
2025-03-07 10:47:34 +0000

    update project

--- PRJ-Portfolio/functions_etf.py
+++ PRJ-Portfolio/functions_etf.py
@@ -12,6 +12,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from pathlib import Path
 import os
 import shutil
