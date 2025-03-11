64b0fa088dceff26787b22126e7aae1fd83b1227
roberto <roberto.polegato@libero.it>
2025-03-11 10:42:41 +0000

    update project

--- PRJ-Portfolio/functions_etf.py
+++ PRJ-Portfolio/functions_etf.py
@@ -276,7 +276,7 @@ def listStocksCountries(isin):
     driverExt.set_page_load_timeout(30)  # Timeout per il caricamento della pagina
     driverExt.get(URL)
     time.sleep(10)
-    print('Inizio il test di lettura per isin_ {isin} al link {URK}:')
+    print('Inizio il test di lettura:')
     try:
       testvalue = WebDriverWait(driverExt, 20).until(EC.presence_of_element_located((By.XPATH,'/html/body/app-root/app-page-template/main/app-stock-profile/div[1]/div[1]/div[1]/div/div[1]/h1'))).get_attribute("innerText")
       print(f'Test lettura da pagina: {testvalue}')
