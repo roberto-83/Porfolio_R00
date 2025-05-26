11c129dde5f638aa3b9f8f3a0ab5282be486039a
roberto <roberto.polegato@libero.it>
2025-05-26 10:36:34 +0000

    update project

--- PRJ-Portfolio/functions_bonds.py
+++ PRJ-Portfolio/functions_bonds.py
@@ -214,6 +214,8 @@ def readEuronextREV2(isin, data):
       i += 1
     #print(histpriceExt)
     driverExt.quit()
+    os.system("pkill chromedriver")
+    os.system("pkill chrome")
     #provo  a dare tempo dopo la chiusura per vedere che chiusa tutto e non rimanga appeso
     time.sleep(5)
 
