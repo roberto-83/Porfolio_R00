0db3be44b6aed9743ce655df404aff592030f55b
roberto <roberto.polegato@libero.it>
2025-05-30 07:57:42 +0000

    update project

--- PRJ-Portfolio/functions_bonds.py
+++ PRJ-Portfolio/functions_bonds.py
@@ -267,7 +267,7 @@ def readEuronextREV2(isin, data):
       finally:
           del driverExtBtp
     #provo  a dare tempo dopo la chiusura per vedere che chiusa tutto e non rimanga appeso
-    time.sleep(5)
+    #time.sleep(5)
 
   return histpriceExt
 #print(readEuronextREV2('IT0005580003','08/05/2024'))
