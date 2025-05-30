3189b26e8f304246b7830b5f6d7a03d78d33f5d8
roberto <roberto.polegato@libero.it>
2025-05-30 12:07:29 +0000

    update project

--- PRJ-Portfolio/functions_bonds.py
+++ PRJ-Portfolio/functions_bonds.py
@@ -130,14 +130,22 @@ def readEuronextREV2(isin, data):
   chrome_options = Options()
   chrome_options.add_argument('--headless=new')  # Esegui Chrome in modalità headless
   chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument("--start-maximized")
+
   chrome_options.add_argument("--enable-javascript")
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  chrome_options.add_argument("enable-automation")
-  chrome_options.add_argument("--disable-extensions")
-  chrome_options.add_argument("--dns-prefetch-disable")
+  chrome_options.add_argument("--enable-automation")
+
   chrome_options.add_argument("--disable-gpu")
-  chrome_options.add_argument("--window-size=1920,1080")
   chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+  chrome_options.add_argument("--disable-extensions")
+  chrome_options.add_argument("--disable-software-rasterizer")
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.add_argument("--dns-prefetch-disable")
+  chrome_options.add_argument("--disable-setuid-sandbox")
+
+  
+  chrome_options.add_argument("--window-size=1920,1080")
+  chrome_options.add_argument("--remote-debugging-port=9222")  # <- necessario per evitare il DevToolsActivePort error
   chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
   #chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
 
